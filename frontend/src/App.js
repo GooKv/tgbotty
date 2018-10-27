3e30d2beddb2786a444746ec80fee6d4879c33ca
Daniil Abramov <adaniil4.93@gmail.com>
2018-10-28 00:10:05 +0700

    add proxy for development + add request from main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    message: '',
+  };
+
+  componentDidMount() {
+    this.hello();
+  }
+
+  hello = () => {
+    const body = new FormData();
+    body.append('name', 'whoever u are');
+
+    fetch('/hi', { method: 'POST', body })
+      .then(response => response.json())
+      .then(({ message }) => {
+        this.setState({ message: message });
+      })
+      .catch(console.error);
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.state.message}</p>
         </header>
       </div>
     );
