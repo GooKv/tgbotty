04c44bf3c3a58ac152a907662ba39ed062e42967
GooKv <gookven@gmail.com>
2018-10-27 18:13:41 +0700

    minimal controller

---
+++ src/main/java/team/guest/tgbotty/TgBottyApplication.java
@@ -0,0 +1,11 @@
+package team.guest.tgbotty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBottyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TgBottyApplication.class, args);
+    }
+}
