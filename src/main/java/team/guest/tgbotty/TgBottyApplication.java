2b204ba97eddd7dacb5cd46e5d38154b69cdb663
GooKv <gookven@gmail.com>
2018-10-30 01:48:27 +0700

    #8 telegrambots, controller, bot self-register, starter is broken - won't use

--- src/main/java/team/guest/tgbotty/TgBottyApplication.java
+++ src/main/java/team/guest/tgbotty/TgBottyApplication.java
@@ -1,27 +1,13 @@
 package team.guest.tgbotty;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class TgBottyApplication {
-    private static final String URL_FORMAT = "https://api.telegram.org/bot%s/";
-
-    @Value("${tg.bot.api.key}")
-    private String tgBotApiKey;
-
     public static void main(String[] args) {
+        ApiContextInitializer.init();
         SpringApplication.run(TgBottyApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate tgRestTemplate(RestTemplateBuilder builder) {
-        return builder
-                .rootUri(String.format(URL_FORMAT, tgBotApiKey))
-                .build();
     }
 }
