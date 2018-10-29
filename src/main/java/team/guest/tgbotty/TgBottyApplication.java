1021ee83b904d53cb026538aa0763b470fb142c1
GooKv <gookven@gmail.com>
2018-10-30 01:53:17 +0700

    #8 redundant init removed (no polling)

--- src/main/java/team/guest/tgbotty/TgBottyApplication.java
+++ src/main/java/team/guest/tgbotty/TgBottyApplication.java
@@ -2,12 +2,10 @@ package team.guest.tgbotty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class TgBottyApplication {
     public static void main(String[] args) {
-        ApiContextInitializer.init();
         SpringApplication.run(TgBottyApplication.class, args);
     }
 }
