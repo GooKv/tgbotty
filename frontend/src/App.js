01eba6a87eab80370d97eec3754d59ee1b1952ff
Eliseev Aleksei <eliseev-eag@yandex.ru>
2018-11-01 15:58:57 +0700

    Сверстал заготовку для шаблона

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
-import { Layout, Row, Col } from 'antd';
+import React, { Component } from "react";
+import { Layout, Row, Col, Icon } from "antd";
+import "./App.scss";
 
-const { Header, Footer, Content } = Layout;
+const { Header, Footer, Content, Sider } = Layout;
 
+const SiderItemStub = () => (
+  <div
+    style={{
+      width: "100%",
+      height: "30px",
+      background: "yellowgreen",
+      margin: "10px 0px"
+    }}
+  />
+);
 class App extends Component {
   state = {
-    message: '',
+    message: ""
   };
 
   componentDidMount() {
@@ -14,9 +25,9 @@ class App extends Component {
 
   hello = () => {
     const body = new FormData();
-    body.append('name', 'whoever u are');
+    body.append("name", "whoever u are");
 
-    fetch('/hi', { method: 'POST', body })
+    fetch("/hi", { method: "POST", body })
       .then(response => response.json())
       .then(({ message }) => {
         this.setState({ message: message });
@@ -26,18 +37,45 @@ class App extends Component {
 
   render() {
     return (
-      <Layout style={{ height: '100vh' }}>
-        <Header>
-          <h2 style={{ color: 'white' }}>tgbotty</h2>
-        </Header>
+      <Layout className="main-layout">
+        <Sider
+          className="main-sider"
+          width={400}
+          collapsible
+          trigger={<Icon type="menu-fold" theme="outlined" />}
+        >
+          {Array(100)
+            .fill()
+            .map((_, index) => (
+              <SiderItemStub key={index} />
+            ))}
+        </Sider>
         <Content>
-          <Row>
-            <Col>
-              {this.state.message}
-            </Col>
-          </Row>
+          <Layout className="main-layout">
+            <Header className="main-header">
+              <h2 style={{ color: "white" }}>tgbotty</h2>
+            </Header>
+            <Content className="main-content">
+              {Array(100)
+                .fill()
+                .map((_, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      height: "100px",
+                      background: "black",
+                      margin: "10px 0"
+                    }}
+                  />
+                ))}
+
+              <Row>
+                <Col>{this.state.message}</Col>
+              </Row>
+            </Content>
+            <Footer className="main-footer">Footer</Footer>
+          </Layout>
         </Content>
-        <Footer>Footer</Footer>
       </Layout>
     );
   }
