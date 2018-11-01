da48b42be79df3ab0542dbe226f0f5fe4e953e10
caseus-lib <lozh.daria@gmail.com>
2018-11-01 17:51:26 +0700

    fix db

--- src/main/java/team/guest/tgbotty/controller/NoChatFoundException.java
+++ src/main/java/team/guest/tgbotty/controller/NoChatFoundException.java
@@ -2,7 +2,7 @@ package team.guest.tgbotty.controller;
 
 public class NoChatFoundException extends RuntimeException {
 
-    private static final String MESSAGE = "Не найден час с id %d";
+    private static final String MESSAGE = "Не найден чат с id %d";
 
     public NoChatFoundException(Long id) {
         super(String.format(MESSAGE, id));
