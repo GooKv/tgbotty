b8dd80ca285e87a0e6e2a92cf7e5bbb027892c84
GooKv <gookven@gmail.com>
2018-11-01 09:51:57 +0300

    bpmn starter

---
+++ src/main/java/team/guest/tgbotty/bot/BotProcessScriptsFacade.java
@@ -0,0 +1,24 @@
+package team.guest.tgbotty.bot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.inject.Named;
+
+@Named("bot")
+@Component
+public class BotProcessScriptsFacade {
+    private final AbsSender sender;
+
+    @Autowired
+    public BotProcessScriptsFacade(AbsSender sender) {
+        this.sender = sender;
+    }
+
+    public void sendSimpleMessage(Long chatId, String message) throws TelegramApiException {
+        sender.execute(new SendMessage(chatId, message));
+    }
+}
--- src/main/java/team/guest/tgbotty/controller/CustomTgRestController.java
+++ src/main/java/team/guest/tgbotty/controller/CustomTgRestController.java
@@ -3,9 +3,7 @@ package team.guest.tgbotty.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiValidationException;
 import org.telegram.telegrambots.meta.generics.WebhookBot;
 
 import java.util.Arrays;
@@ -15,10 +13,12 @@ import java.util.stream.Collectors;
 
 @RestController("/callback")
 public class CustomTgRestController {
+    private final ExampleProcessStarter exampleProcessStarter;
     private final Map<String, WebhookBot> registeredBots;
 
     @Autowired
-    public CustomTgRestController(WebhookBot... webHookBots) {
+    public CustomTgRestController(ExampleProcessStarter exampleProcessStarter, WebhookBot... webHookBots) {
+        this.exampleProcessStarter = exampleProcessStarter;
         registeredBots = Arrays.stream(webHookBots)
                 .collect(Collectors.toMap(WebhookBot::getBotPath, Function.identity()));
     }
@@ -31,16 +31,8 @@ public class CustomTgRestController {
             throw new BotNotFoundException(path);
         }
 
-        BotApiMethod response = webhookBot.onWebhookUpdateReceived(update);
-
-        try {
-            if (response != null) {
-                response.validate();
-            }
-            return response;
-        } catch (TelegramApiValidationException exception) {
-            throw new IncorrectBotResponseException(path, response, exception);
-        }
+        exampleProcessStarter.startRepeaterProcess(update.getMessage().getChatId(), update);
+        return null;
     }
 
     @GetMapping(value = "/callback/{path}", produces = MediaType.APPLICATION_JSON_VALUE)
